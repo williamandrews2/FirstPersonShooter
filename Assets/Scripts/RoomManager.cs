63b2a142d29ab5948b657deee3df1d47790800a7
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-05-16 15:54:50 -0500

    Created PlayerManager gameobject instantiation for players as well as add Resources folder for Photon.

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+
+    private void Awake()
+    {
+        // Check if another RoomManager exists, and if so, destroy it.
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex == 1) // 1 is the index of the Game scene in the build settings.
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
+    }
+}

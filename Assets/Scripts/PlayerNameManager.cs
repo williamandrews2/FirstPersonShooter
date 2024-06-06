efd0d69a63575544f33d18d8498bd4c573d8e503
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-06-06 17:35:18 -0500

    Added PlayerPrefs for usernames and fixed bug that allowed players to have no username.

--- Assets/Scripts/PlayerNameManager.cs
+++ Assets/Scripts/PlayerNameManager.cs
@@ -8,8 +8,33 @@ public class PlayerNameManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField usernameInput;
 
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("username"))
+        {
+            usernameInput.text = PlayerPrefs.GetString("username");
+            PhotonNetwork.NickName = PlayerPrefs.GetString("username");
+        }
+        else
+        {
+            SetRandomUsername();
+        }
+    }
+
     public void OnUsernameInputValueChanged()
     {
+        if(usernameInput.text == "")
+        {
+            SetRandomUsername();
+        }
+
         PhotonNetwork.NickName = usernameInput.text;
+        PlayerPrefs.SetString("username", usernameInput.text);
+    }
+
+    void SetRandomUsername()
+    {
+        usernameInput.text = "Player" + Random.Range(0, 10000).ToString("0000");
+        OnUsernameInputValueChanged(); // Save the random username so we do not generate a random one each time.
     }
 }

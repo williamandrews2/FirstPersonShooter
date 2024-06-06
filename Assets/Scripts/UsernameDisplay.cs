6fb32186a7387af88cb2c7659641fad5cb5860de
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-06-06 17:06:58 -0500

    Created username display above players in game.

---
+++ Assets/Scripts/UsernameDisplay.cs
@@ -0,0 +1,16 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UsernameDisplay : MonoBehaviour
+{
+    [SerializeField] PhotonView playerPV;
+    [SerializeField] TMP_Text text;
+
+    private void Start()
+    {
+        text.text = playerPV.Owner.NickName;
+    }
+}
